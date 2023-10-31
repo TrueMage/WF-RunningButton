68475964111b6911202dd090f6e662cc0c3be533
TrueMage <truefalc@gmail.com>
2023-10-31 23:12:53 +0100

    update

--- WF-RunningButton/Form1.cs
+++ WF-RunningButton/Form1.cs
@@ -2,9 +2,15 @@ namespace WF_RunningButton
 {
     public partial class Form1 : Form
     {
+        Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.Location = new Point(rand.Next(0, this.Width - 150), rand.Next(0, this.Height - 100));
         }
     }
 }
